e83ed01066c7ce242d6db8db043c4e7dffccbbb3
jorge <jorgegarciahinestrosa@gmail.com>
2015-08-17 21:40:26 +0200

    Add grunt-gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,11 +11,18 @@ module.exports = function(grunt) {
           {expand: true, src: ['*.html', 'scripts/*', 'styles/*'], dest: 'dist/'}
         ]
       }
+    },
+    'gh-pages': {
+      options: {
+        base: 'dist'
+      },
+      src: ['**']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-gh-pages');
 
-  grunt.registerTask('build', ['copy:dist']);
+  grunt.registerTask('build', ['copy:dist', 'gh-pages']);
 
 };
