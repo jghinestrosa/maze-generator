99a7d4e97afc963b514fb2fa07c8bf828bcd1352
jorge <jorgegarciahinestrosa@gmail.com>
2015-08-18 21:07:26 +0200

    Add grunt-contrib-clean and create deploy task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,13 @@
-'use strict';
+',use strict';
 
 module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    clean: {
+      dist: ['dist']
+    },
 
     copy: {
       dist: {
@@ -12,6 +16,7 @@ module.exports = function(grunt) {
         ]
       }
     },
+
     'gh-pages': {
       options: {
         base: 'dist'
@@ -20,9 +25,11 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-gh-pages');
 
-  grunt.registerTask('build', ['copy:dist', 'gh-pages']);
+  grunt.registerTask('build', ['clean:dist', 'copy:dist']);
+  grunt.registerTask('deploy', ['gh-pages']);
 
 };
